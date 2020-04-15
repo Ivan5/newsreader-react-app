275454fec490468282b6cd44c08008dcd61622e8
Ivan5 <converse_ivan@hotmail.com>
2020-04-15 16:15:01 -0500

    Adding our container styles to StoriesContainer.js. Close #8

--- src/containers/StoriesContainer.js
+++ src/containers/StoriesContainer.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { getStoryIds } from "../services/hnApi";
 import Story from "../components/Story";
+import {
+  GlobalStyle,
+  StoriesContainerWrapper,
+} from "../styles/StoriesContainerStyles";
 
 export default function StoriesContainer() {
   const [storyIds, setStoryIds] = useState([]);
@@ -11,10 +15,13 @@ export default function StoriesContainer() {
 
   return (
     <>
-      <h1>Hacker News Stories</h1>
-      {storyIds.map((storyId) => (
-        <Story key={storyId} storyId={storyId} />
-      ))}
+      <GlobalStyle />
+      <StoriesContainerWrapper data-test-id="stories-container">
+        <h1>Hacker News Stories</h1>
+        {storyIds.map((storyId) => (
+          <Story key={storyId} storyId={storyId} />
+        ))}
+      </StoriesContainerWrapper>
     </>
   );
 }
