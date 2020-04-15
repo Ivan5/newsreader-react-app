bfb8ce6fbebd3b7e35e1f0e59dccb10737429dc1
Ivan5 <converse_ivan@hotmail.com>
2020-04-15 12:24:12 -0500

    Creating hnApi service file. Close #2

---
+++ src/services/hnApi.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export const baseURl = "https://hacker-news.firebaseio.com/v0/";
+
+export const newStoriesUrl = `${baseURl}newstories.json`;
+export const storyUrl = `${baseURl}item/`;
+
+export const getStoryIds = async () => {
+  const result = await axios.get(newStoriesUrl).then((data) => data);
+
+  return result;
+};
