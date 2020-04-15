c5b6ae91a22a09bfcf475592f67fad2ceb8dc6e2
Ivan5 <converse_ivan@hotmail.com>
2020-04-15 13:15:07 -0500

    Start work in App.js and implements Hacker News API. Close #3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getStoryIds } from "./services/hnApi";
 
 function App() {
+  const [storyIds, setStoryIds] = useState([]);
+
+  useEffect(() => {
+    getStoryIds().then((data) => setStoryIds(data));
+  }, []);
+
   return (
     <div className="App">
-      <p></p>
+      <p>{JSON.stringify(storyIds)}</p>
     </div>
   );
 }
--- src/services/hnApi.js
+++ src/services/hnApi.js
@@ -6,7 +6,7 @@ export const newStoriesUrl = `${baseURl}newstories.json`;
 export const storyUrl = `${baseURl}item/`;
 
 export const getStoryIds = async () => {
-  const result = await axios.get(newStoriesUrl).then((data) => data);
+  const result = await axios.get(newStoriesUrl).then(({ data }) => data);
 
   return result;
 };
